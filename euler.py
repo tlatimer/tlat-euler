dfa1e9c462ff9855b33313a3144ad9c850fb2a4e
tlatimer <tom@tomlat.com>
2019-04-02 11:49:45 -0500

    test2

--- euler.py
+++ euler.py
@@ -1,1 +1,9 @@
-print('hello world2')
+def problem001():
+    """Find the sum of all the multiples of 3 or 5 below 1000"""
+    multSum = 0
+    for i in range(1000):
+        if i % 3 == 0 or i %5 == 0:
+            multSum += i
+    return multSum
+
+print(problem001())
